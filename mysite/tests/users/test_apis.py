9664a8c69ab663ce1a1ddcd324d2e2712d2faaf1
yamkant <dev.yamkim@gmail.com>
2023-11-12 22:50:50 +0900

    feat: tutorial을 따라서 기본적인 설정 및 테스트 추가

---
+++ mysite/tests/users/test_apis.py
@@ -0,0 +1,114 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from apps.users import apis
+from apps.sql_app import models
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+app = apis.app
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+models.Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture()
+def test_db(test_db):
+    models.Base.metadata.create_all(bind=engine)
+    yield
+    models.Base.metadata.drop_all(bind=engine)
+
+app.dependency_overrides[apis.get_db] = override_get_db
+
+client = TestClient(app)
+
+def test_create_user():
+    response = client.post(
+        "/users/",
+        json={"email": "dev.yamkim@gmail.com", "password": "test123!"},
+    )
+    assert response.status_code == 200, response.text
+    print(response.json())
+
+    # data = response.json()
+    # assert data["email"] == "deadpool@example.com"
+    # assert "id" in data
+    # user_id = data["id"]
+
+    # response = client.get(f"/users/{user_id}")
+    # assert response.status_code == 200, response.text
+    # data = response.json()
+    # assert data["email"] == "deadpool@example.com"
+    # assert data["id"] == user_id
+
+
+# def test_read_item():
+#     response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
+#     assert response.status_code == 200
+#     assert response.json() == {
+#         "id": "foo",
+#         "title": "Foo",
+#         "description": "There goes my hero",
+#     }
+
+
+# def test_read_item_bad_token():
+#     response = client.get("/items/foo", headers={"X-Token": "hailhydra"})
+#     assert response.status_code == 400
+#     assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+# def test_read_inexistent_item():
+#     response = client.get("/items/baz", headers={"X-Token": "coneofsilence"})
+#     assert response.status_code == 404
+#     assert response.json() == {"detail": "Item not found"}
+
+
+# def test_create_item():
+#     response = client.post(
+#         "/items/",
+#         headers={"X-Token": "coneofsilence"},
+#         json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
+#     )
+#     assert response.status_code == 200
+#     assert response.json() == {
+#         "id": "foobar",
+#         "title": "Foo Bar",
+#         "description": "The Foo Barters",
+#     }
+
+
+# def test_create_item_bad_token():
+#     response = client.post(
+#         "/items/",
+#         headers={"X-Token": "hailhydra"},
+#         json={"id": "bazz", "title": "Bazz", "description": "Drop the bazz"},
+#     )
+#     assert response.status_code == 400
+#     assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+# def test_create_existing_item():
+#     response = client.post(
+#         "/items/",
+#         headers={"X-Token": "coneofsilence"},
+#         json={
+#             "id": "foo",
+#             "title": "The Foo ID Stealers",
+#             "description": "There goes my stealer",
+#         },
+#     )
+#     assert response.status_code == 400
+#     assert response.json() == {"detail": "Item already exists"}
