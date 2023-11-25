4c19577eb66dad356a8140dbb36237ae9188882a
yamkant <dev.yamkim@gmail.com>
2023-11-26 08:51:01 +0900

    feat: [frontend] [Component: post] 주기적으로 뉴스의 타이틀을 갱신하는 배너 추가

--- frontend/app/src/components/PostApp.vue
+++ frontend/app/src/components/PostApp.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="bottom-banner" tabindex="-1"
-    class="fixed bottom-0 start-0 z-50 flex justify-between w-full p-4 border-t border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-    <div class="flex items-center mx-auto">
+  <div
+    id="bottom-banner"
+    tabindex="-1"
+    class="fixed bottom-0 start-0 z-50 flex justify-between w-full p-4 border-t border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
+  >
+    <div class="flex items-center w-4/5 mx-auto">
       <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400 space-x-2">
-        <font-awesome-icon icon="fa-solid fa-money-bill-trend-up" />
+        <font-awesome-icon icon="fa-solid fa-money-bill-trend-up" class="mr-2" />
         <span>{{ title }}
           <a href="https://flowbite.com"
             class="flex items-center ms-0 text-sm font-medium text-blue-600 md:ms-1 md:inline-flex dark:text-blue-500 hover:underline space-x-2"
@@ -26,12 +29,42 @@
 </template>
   
 <script>
+import axiosInstance from "../libs"
+
 export default {
   data() {
     return {
-      title: "This is space for a news title.",
+      title: "",
+      news: [],
+      current_index: 0,
     };
   },
+  mounted() {
+    this.getNewsPosts();
+    setInterval(this.updateTitle, 4000);;
+  },
+  methods: {
+    async getNewsPosts() {
+      try {
+        const response = await axiosInstance.get("/posts/news");
+        this.news = this.shuffle(response.data);
+        this.title = this.news[0].title;
+      } catch (err) {
+        console.error("Error fetching todos:", err);
+      }
+    },
+    updateTitle() {
+      this.current_index = (this.current_index + 1) % this.news.length;
+      this.title = this.news[this.current_index].title
+    },
+    shuffle(array) { 
+      for (let i = array.length - 1; i > 0; i--) { 
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]]; 
+      } 
+      return array; 
+    }
+  }
 };
 </script>
   
