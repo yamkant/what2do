38bacbd520c1453837792495ac4a7ddac0d82df9
yamkant <dev.yamkim@gmail.com>
2023-11-25 23:19:46 +0900

    feat: [backend] [APP: news] ComponentParser 리팩토링 (주입하는 형식으로)

--- mysite/project/apps/news/main.py
+++ mysite/project/apps/news/main.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 import json
 
-from driver_helper import DriverHelper
-from news_parser import NewsParser
+from classes.driver_helper import DriverHelper
+from classes.news_parser import NewsParser
+from classes.component_parser import PostComponentParser
 
 def main():
     driver: webdriver = webdriver.Chrome()
     url: str = 'https://finance.yahoo.com/topic/stock-market-news/'
-    newsParser: NewsParser = NewsParser(driver, url, DriverHelper(driver))
+    driver.get(url)
+
+    newsParser: NewsParser = NewsParser(driver, DriverHelper(driver), PostComponentParser())
     ret_json = newsParser.run()
     with open('./data/output.json', 'w', encoding='utf-8') as json_file:
         json.dump(ret_json, json_file, ensure_ascii=False, indent=2)
